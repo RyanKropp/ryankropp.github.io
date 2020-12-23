9a5e509aa267e8671d3c3b9bc796404267b621e3
Ryan Kropp <50561790+RyanKropp@users.noreply.github.com>
2020-12-23 15:02:49 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ document.getElementById("body").onscroll = function myFunction() {
     var scrolltotop = document.scrollingElement.scrollTop;
     var target = document.getElementById("parallax_img");
     var xvalue = "center";
-    var factor = -0.15;
+    var factor = -0.5;
     var yvalue = scrolltotop * factor;
     target.style.backgroundPosition = xvalue + " " + yvalue + "px";
   }
