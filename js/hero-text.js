5b3af714eadcbeb92e48531700383cf60c12f828
Ryan Kropp <50561790+RyanKropp@users.noreply.github.com>
2021-01-20 21:44:29 -0500

    Update hero-text.js

--- js/hero-text.js
+++ js/hero-text.js
@@ -1,7 +1,7 @@
 var text = ["design", "develop", "streamline", "create", "innovate", "prototype", "automate"];
 var counter = 0;
 var elem = document.getElementById("hero-text-c");
-var inst = setInterval(change, 1500);
+var inst = setInterval(change, 2000);
 
 function change() {
   elem.innerHTML = text[counter];
