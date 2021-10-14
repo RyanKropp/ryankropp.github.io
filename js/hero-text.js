1a59512b72fef345520f4fa066afcb0ef5f7eb5e
Ryan Kropp <50561790+RyanKropp@users.noreply.github.com>
2021-10-13 23:13:18 -0400

    Update hero-text.js

--- js/hero-text.js
+++ js/hero-text.js
@@ -1,6 +1,6 @@
 var text = ["design", "develop", "streamline", "create", "innovate", "prototype", "automate"];
 var counter = 0;
-var elem = document.getElementById("hero-text-c");
+var elem = document.getElementById("slide-up-out");
 var inst = setInterval(change, 2000);
 
 function change() {
